f2a12bf9d812cb5685cf71e5a74564fe7111c5fe
maheshnnaik <36007557+maheshnnaik@users.noreply.github.com>
2024-09-18 19:43:20 +0530

    Update Alert.js

--- frontend/src/components/Alert.js
+++ frontend/src/components/Alert.js
@@ -1,3 +1,6 @@
+import { InformationCircleIcon } from "@heroicons/react/16/solid";
+import { XCircleIcon } from "@heroicons/react/24/outline";
+
 export const Alert = ({ position, data, hideAlert }) => {
     // style this component based on position, position will be string - botton-left, bottom
     const positionStyle = position === 'bottom-left' ? 'bottom-0 left-0' : 'bottom-0 right-0';
@@ -5,9 +8,7 @@ export const Alert = ({ position, data, hideAlert }) => {
 
     return (
         <div className={`absolute flex p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 ${positionStyle}`} role="alert" id="alert-1">
-            <svg className="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-            </svg>
+            <InformationCircleIcon width={20} height={20}/>
             <span className="sr-only">Info</span>
             <div>
                 <span className="font-medium">Pipeline Info:</span>
@@ -25,9 +26,7 @@ export const Alert = ({ position, data, hideAlert }) => {
                 onClick={hideAlert}
             >
                 <span class="sr-only">Close</span>
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
+                <XCircleIcon width={20} height={20}/>
             </button>
         </div>
     )
